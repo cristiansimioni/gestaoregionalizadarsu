Fix outbound calculation for existent landfill

src/combinations/combinations.py
@@ -184,7 +184,7 @@ def calculateInboundOutbound(cdata, distance, subarray, isCentralized, utvrs_onl
                 transshipment_cost = cdata[other_city]["transshipment-cost"]
                 cost_post_transhipment = cdata[other_city]["cost-post-transhipment"]
                 trash = cdata[other_city]["trash"]
-                sum_inbound = sum_inbound + round(((conventional_cost) + (transshipment_cost) + (cost_post_transhipment * getDistanceBetweenCites(cdata, distance, other_city, utvr_city))) * trash, 3)
+                sum_inbound = sum_inbound + round(((conventional_cost) + (transshipment_cost) + (cost_post_transhipment * getDistanceBetweenCites(cdata, distance, other_city, utvr_city))) * trash, ROUND)
                 #sum_co2 = sum_co2 + (1.24 * getDistanceBetweenCites(cdata, distance, utvr_city, other_city * trash))
 
             #sum_co2 = sum_co2 / getSubTrash(cdata, subarray)
@@ -198,14 +198,14 @@ def calculateInboundOutbound(cdata, distance, subarray, isCentralized, utvrs_onl
                 if distCities < dist:
                     dist = distCities
                     sum_outbound = 0
-                    entry['outbound-existent-landfill'] = sum_outbound + (distCities * (MOVIMENTATION_COST * cdata[utvr_city]["cost-post-transhipment"])) * LANDFILL_DEVIATION
+                    entry['outbound-existent-landfill'] = sum_outbound + round((distCities * (MOVIMENTATION_COST * cdata[utvr_city]["cost-post-transhipment"])) * LANDFILL_DEVIATION, ROUND)
                     entry["aterro-existente"] = a
         
             for a in aterros_only:
                 e = copy.deepcopy(entry)
                 sum_outbound = 0
                 #sum_outbound = sum_outbound + (getDistanceBetweenCites(cdata, distance, utvr_city,a) * (0.7 * cdata[utvr_city]["cost-post-transhipment"])) * LANDFILL_DEVIATION
-                e["outbound"] = ((subArrayTrash * LANDFILL_DEVIATION)*(cdata[utvr_city]["cost-post-transhipment"]*getDistanceBetweenCites(cdata, distance, utvr_city,a)*MOVIMENTATION_COST))/subArrayTrash
+                e["outbound"] = round(((subArrayTrash * LANDFILL_DEVIATION)*(cdata[utvr_city]["cost-post-transhipment"]*getDistanceBetweenCites(cdata, distance, utvr_city,a)*MOVIMENTATION_COST))/subArrayTrash, ROUND)
                 e["aterro"] = a
                 e["total"] = e["inbound"] + e["outbound"]
                 
@@ -326,7 +326,7 @@ def main():
     report.write("Capex Outbound: " + repr(CAPEX_OUTBOUND) + "\n")
     report.write("Prazo: " + repr(PAYMENT_PERIOD) + "\n")
     report.write("Desvio Aterro (%): " + repr(LANDFILL_DEVIATION) + "\n")
-    report.write("Redução Custo de Transporte (%): " + repr(MOVIMENTATION_COST) + "\n\n\n")
+    report.write("Redução Custo de Transporte (%): " + repr(MOVIMENTATION_COST) + "\n")
 
     # RSU
     rsutrash = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335, 340, 345, 350, 355, 360, 365, 370, 375, 380, 385, 390, 395, 400, 405, 410, 415, 420, 425, 430, 435, 440, 445, 450, 455, 460, 465, 470, 475, 480, 485, 490, 495, 500, 505, 510, 515, 520, 525, 530, 535, 540, 545, 550, 555, 560, 565, 570, 575, 580, 585, 590, 595, 600, 605, 610, 615, 620, 625, 630, 635, 640, 645, 650, 655, 660, 665, 670, 675, 680, 685, 690, 695, 700, 705, 710, 715, 720, 725, 730, 735, 740, 745, 750, 755, 760, 765, 770, 775, 780, 785, 790, 795, 800, 805, 810, 815, 820, 825, 830, 835, 840, 845, 850, 855, 860, 865, 870, 875, 880, 885, 890, 895, 900, 905, 910, 915, 920, 925, 930, 935, 940, 945, 950, 955, 960, 965, 970, 975, 980, 985, 990, 995, 1000, 1005, 1010, 1015, 1020, 1025, 1030, 1035, 1040, 1045, 1050, 1055, 1060, 1065, 1070, 1075, 1080, 1085, 1090, 1095, 1100, 1105, 1110, 1115, 1120, 1125, 1130, 1135, 1140, 1145, 1150, 1155, 1160, 1165, 1170, 1175, 1180, 1185, 1190, 1195, 1200, 1205, 1210, 1215, 1220, 1225, 1230, 1235, 1240, 1245, 1250, 1255, 1260, 1265, 1270, 1275, 1280, 1285, 1290, 1295, 1300, 1305, 1310, 1315, 1320, 1325, 1330, 1335, 1340, 1345, 1350, 1355, 1360, 1365, 1370, 1375, 1380, 1385, 1390, 1395, 1400, 1405, 1410, 1415, 1420, 1425, 1430, 1435, 1440, 1445, 1450, 1455, 1460, 1465, 1470, 1475, 1480, 1485, 1490, 1495, 1500, 1505, 1510, 1515, 1520, 1525, 1530, 1535, 1540, 1545, 1550, 1555, 1560, 1565, 1570, 1575, 1580, 1585, 1590, 1595, 1600, 1605, 1610, 1615, 1620, 1625, 1630, 1635, 1640, 1645, 1650, 1655, 1660, 1665, 1670, 1675, 1680, 1685, 1690, 1695, 1700, 1705, 1710, 1715, 1720, 1725, 1730, 1735, 1740, 1745, 1750, 1755, 1760, 1765, 1770, 1775, 1780, 1785, 1790, 1795, 1800, 1805, 1810, 1815, 1820, 1825, 1830, 1835, 1840, 1845, 1850, 1855, 1860, 1865, 1870, 1875, 1880, 1885, 1890, 1895, 1900, 1905, 1910, 1915, 1920, 1925, 1930, 1935, 1940, 1945, 1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045, 2050, 2055, 2060, 2065, 2070, 2075, 2080, 2085, 2090, 2095, 2100, 2105, 2110, 2115, 2120, 2125, 2130, 2135, 2140, 2145, 2150, 2155, 2160, 2165, 2170, 2175, 2180, 2185, 2190, 2195, 2200, 2205, 2210, 2215, 2220, 2225, 2230, 2235, 2240, 2245, 2250, 2255, 2260, 2265, 2270, 2275, 2280, 2285, 2290, 2295, 2300, 2305, 2310, 2315, 2320, 2325, 2330, 2335, 2340, 2345, 2350, 2355, 2360, 2365, 2370, 2375, 2380, 2385, 2390, 2395, 2400, 2405, 2410, 2415, 2420, 2425, 2430, 2435, 2440, 2445, 2450, 2455, 2460, 2465, 2470, 2475, 2480, 2485, 2490, 2495, 2500]
@@ -341,8 +341,8 @@ def main():
             city = {}
             city["name"] = row["Município"]
             city["position"] = line_count
-            city["population"] = round(float(row["População"]), 3)
-            city["trash"] = round(float(row["Lixo (t/d)"]), 3)
+            city["population"] = round(float(row["População"]), ROUND)
+            city["trash"] = round(float(row["Lixo (t/d)"]), ROUND)
             if row["UTVR"] == "Sim":
                 city["utvr"] = True
             else:
@@ -355,9 +355,9 @@ def main():
                 city["existent-landfill"] = True
             else:
                 city["existent-landfill"] = False
-            city["conventional-cost"] = round(float(row["Custo de Coleta Mista Convencional"]), 3)
-            city["transshipment-cost"] = round(float(row["Custo de Coleta e Transbordo de Resíduos Mistos"]), 3)
-            city["cost-post-transhipment"] = round(float(row["Custo de Transporte Pós Transbordo"]), 3)
+            city["conventional-cost"] = round(float(row["Custo de Coleta Mista Convencional"]), ROUND)
+            city["transshipment-cost"] = round(float(row["Custo de Coleta e Transbordo de Resíduos Mistos"]), ROUND)
+            city["cost-post-transhipment"] = round(float(row["Custo de Transporte Pós Transbordo"]), ROUND)
             citiesdic[city["name"]] = city
             totalTrash = totalTrash + city["trash"]
             totalPopulation = totalPopulation + city["population"]
@@ -369,6 +369,12 @@ def main():
     #for r in reader:
     #    rsu.append(r)
     #    print(r)
+
+    capexInbound  = round((CAPEX_INBOUND  * 1000000)/(totalTrash * 313 * PAYMENT_PERIOD), ROUND)
+    capexOutbound = round((CAPEX_OUTBOUND * 1000000)/(totalTrash * 313 * PAYMENT_PERIOD), ROUND)
+
+    report.write("Capex Inbound: " + repr(capexInbound) + "\n")
+    report.write("Capex Outbound: " + repr(capexOutbound) + "\n\n\n")
 
     # Read distances from CSV file
     distance = np.loadtxt(open(CSVDISTANCE, "rb"), delimiter=",", skiprows=0)
@@ -427,8 +433,6 @@ def main():
 
     logging.info("Cálculando valores (inbound, tecnologia e outbound) por combinação...")
 
-    capexInbound  = (CAPEX_INBOUND  * 1000000)/(totalTrash * 313 * PAYMENT_PERIOD)
-    capexOutbound = (CAPEX_OUTBOUND * 1000000)/(totalTrash * 313 * PAYMENT_PERIOD)
 
     new_comb = list()
     #sub_arrays = set()
@@ -464,14 +468,14 @@ def main():
         # Initialize values for array
         arrayResult['inbound'] = 0
         arrayResult['outbound'] = 0
+        arrayResult['outbound-sum'] = 0
+        arrayResult['outbound-capex'] = 0
         arrayResult['outbound-existent-landfill'] = 0
+        arrayResult['outbound-existent-landfill-sum'] = 0
+        arrayResult['outbound-capex-existent-landfill'] = 0
         arrayResult['technology'] = 0
         arrayResult['inbound-sum'] = 0
-        arrayResult['outbound-sum'] = 0
-        arrayResult['outbound-existent-landfill-sum'] = 0
         arrayResult['inbound-capex'] = 0
-        arrayResult['outbound-capex'] = 0
-        arrayResult['outbound-capex-existent-landfill-sum'] = 0
 
         # Calculate inbound and outbound values without adding the capex
         for subArray in i:
@@ -497,7 +501,7 @@ def main():
                 subArray['inbound-capex'] = 0
             if arrayResult['outbound-sum'] > 0:
                 subArray['outbound-capex'] = subArray['outbound'] + capexOutbound * ((subArray['outbound']*subArray['trash'])/arrayResult['outbound-sum'])
-                subArray['outbound-capex-existent-landfill'] = subArray['outbound-existent-landfill'] + capexOutbound * ((subArray['outbound-existent-landfill']*subArray['trash'])/arrayResult['outbound-sum'])
+                subArray['outbound-capex-existent-landfill'] = subArray['outbound-existent-landfill'] + capexOutbound * ((subArray['outbound-existent-landfill']*subArray['trash'])/arrayResult['outbound-existent-landfill-sum'])
             else:
                 subArray['outbound-capex'] = 0
                 subArray['outbound-capex-existent-landfill'] = 0
@@ -507,7 +511,7 @@ def main():
             arrayResult['outbound-existent-landfill'] = arrayResult['outbound-existent-landfill'] + (subArray['outbound-existent-landfill'] * subArray['trash'])
             arrayResult['inbound-capex'] = arrayResult['inbound-capex'] + (subArray['inbound-capex'] * subArray['trash'])
             arrayResult['outbound-capex'] = arrayResult['outbound-capex'] + (subArray['outbound-capex'] * subArray['trash'])
-            arrayResult['outbound-capex-existent-landfill'] = arrayResult['outbound-existent-landfill'] + (subArray['outbound-capex-existent-landfill'] * subArray['trash'])
+            arrayResult['outbound-capex-existent-landfill'] = arrayResult['outbound-capex-existent-landfill'] + (subArray['outbound-capex-existent-landfill'] * subArray['trash'])
             arrayResult['technology'] = arrayResult['technology'] + (subArray['technology'] * subArray['trash'])
 
         arrayResult["arranjo"] = i
@@ -541,7 +545,9 @@ def main():
             report.write("-- Inbound - Capex: " + repr(d["inbound-capex"]) + "\n")
             report.write("-- Tecnologia: " + repr(d['technology']) + "\n")
             report.write("-- Outbound: " + repr(d["outbound"]) + "\n")
-            report.write("-- Outbound - Capex: " + repr(d["outbound-capex"]) + "\n\n")
+            report.write("-- Outbound - Capex: " + repr(d["outbound-capex"]) + "\n")
+            report.write("-- Outbound Aterro Existente: " + repr(d["outbound-existent-landfill"]) + "\n")
+            report.write("-- Outbound Aterro Existente - Capex: " + repr(d["outbound-capex-existent-landfill"]) + "\n\n")
             report.write("-- Sub-arranjos:\n")
 
             output.write(repr(d["arranjo"]) + ";Sumário;NA;NA;NA;" + repr(d["population"]) + ";" + repr(d["total-capex"]) + ";" + repr(d["trash"]) + ";" + repr(d['technology']) + ";" + repr(d["inbound-capex"]) + ";" + repr(d["outbound-capex"]) + ";" + repr(d['outbound-capex-existent-landfill']) + "\n")
@@ -563,7 +569,9 @@ def main():
                 report.write("\t\t-- Capex: " + repr(d["sub"][x]["capex"]) + "\n")
                 report.write("\t\t-- Opex: " + repr(d["sub"][x]["opex"]) + "\n")
                 report.write("\t-- Outbound: " + repr(d["sub"][x]["outbound"]) + "\n")
-                report.write("\t-- Outbound - Capex: " + repr(d["sub"][x]["outbound-capex"]) + "\n\n")
+                report.write("\t-- Outbound - Capex: " + repr(d["sub"][x]["outbound-capex"]) + "\n")
+                report.write("\t-- Outbound Aterro Existente: " + repr(d["sub"][x]["outbound-existent-landfill"]) + "\n")
+                report.write("\t-- Outbound Aterro Existente - Capex: " + repr(d["sub"][x]["outbound-capex-existent-landfill"]) + "\n\n")
                 break
 
     report.write("\n\n============= TOP " + repr(MAX_ARRAYS) + " ARRANJOS ============= \n")
@@ -581,7 +589,10 @@ def main():
         report.write("-- Inbound + Capex: " + repr(data[i]["inbound-capex"]) + "\n")
         report.write("-- Tecnologia: " + repr(data[i]['technology']) + "\n")
         report.write("-- Outbound: " + repr(data[i]["outbound"]) + "\n")
-        report.write("-- Outbound + Capex: " + repr(data[i]["outbound-capex"]) + "\n\n")
+        report.write("-- Outbound + Capex: " + repr(data[i]["outbound-capex"]) + "\n")
+        report.write("-- Outbound Aterro Existente: " + repr(data[i]["outbound-existent-landfill"]) + "\n")
+        report.write("-- Outbound Aterro Existente - Capex: " + repr(data[i]["outbound-capex-existent-landfill"]) + "\n\n")
+
         report.write("-- Sub-arranjos:\n")
         for x in range(len(data[i]["sub"])):
             output.write(repr(data[i]["arranjo"]) + ";" + repr(data[i]["sub"][x]["sub-arranjo"]) + ";" + repr(data[i]["sub"][x]["aterro"]) + ";" + repr(data[i]["sub"][x]["aterro-existente"]) + ";" + repr(data[i]["sub"][x]["utvr"]) + ";" + repr(data[i]["sub"][x]['population']) + ";" + repr(data[i]["sub"][x]["total-capex"]) + ";" + repr(data[i]["sub"][x]['trash']) + ";" + repr(data[i]["sub"][x]['technology']) + ";" + repr(data[i]["sub"][x]["inbound-capex"])  + ";" + repr(data[i]["sub"][x]["outbound-capex"]) + ";" + repr(data[i]["sub"][x]['outbound-capex-existent-landfill']) + "\n")
@@ -599,7 +610,9 @@ def main():
             report.write("\t\t-- Capex: " + repr(data[i]["sub"][x]["capex"]) + "\n")
             report.write("\t\t-- Opex: " + repr(data[i]["sub"][x]["opex"]) + "\n")
             report.write("\t-- Outbound: " + repr(data[i]["sub"][x]["outbound"]) + "\n")
-            report.write("\t-- Outbound + Capex: " + repr(data[i]["sub"][x]["outbound-capex"]) + "\n\n")
+            report.write("\t-- Outbound + Capex: " + repr(data[i]["sub"][x]["outbound-capex"]) + "\n")
+            report.write("\t-- Outbound Aterro Existente: " + repr(data[i]["sub"][x]["outbound-existent-landfill"]) + "\n")
+            report.write("\t-- Outbound Aterro Existente + Capex: " + repr(data[i]["sub"][x]["outbound-capex-existent-landfill"]) + "\n\n")
 
         report.write("-----------------------------------------------------------------\n\n")
 